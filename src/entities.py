04625d4885251ccf4ba9d1de2a4c0f0bdb172266
seiiiishinsan <gabinhalloss@gmail.com>
2026-01-09 21:05:59 +0100

    v0.2.1: add sword attack animation

--- src/entities.py
+++ src/entities.py
@@ -14,6 +14,10 @@ class Player(pygame.sprite.Sprite):
         self.frame_index = 0
         self.animation_speed = 0.15
 
+        self.attacking = False
+        self.attack_cooldown = 400  # 400 millisecondes entre deux attaques
+        self.attack_time = 0
+
         # C'est ici que ça plantait : la fonction n'existait pas encore
         self.import_player_assets()
 
@@ -34,36 +38,56 @@ class Player(pygame.sprite.Sprite):
         self.animations = {
             'idle': [],
             'run': [],
+            'attack': []
         }
 
         # On remplit le dictionnaire
         # Assure-toi que les fichiers idle.png et run.png existent bien !
         self.animations['idle'] = import_sprite_strip(os.path.join(character_path, 'idle.png'), 4)
         self.animations['run'] = import_sprite_strip(os.path.join(character_path, 'run.png'), 4)
+        self.animations['attack'] = import_sprite_strip(os.path.join(character_path, 'swordattack.png'), 4)
 
     # ----------------------------------------
 
     def input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_z] or keys[pygame.K_UP]:
-            self.direction.y = -1
-        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
+        if not self.attacking:
+            keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_q] or keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        else:
-            self.direction.x = 0
+            # Mouvements (inchangé)
+            if keys[pygame.K_z] or keys[pygame.K_UP]: self.direction.y = -1
+            elif keys[pygame.K_s] or keys[pygame.K_DOWN]: self.direction.y = 1
+            else: self.direction.y = 0
+
+            if keys[pygame.K_q] or keys[pygame.K_LEFT]: self.direction.x = -1
+            elif keys[pygame.K_d] or keys[pygame.K_RIGHT]: self.direction.x = 1
+            else: self.direction.x = 0
+
+            if keys[pygame.K_SPACE]:
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
+                self.direction.x = 0
+                self.direction.y = 0
+                self.frame_index = 0
 
     def get_status(self):
-        if self.direction.x == 0 and self.direction.y == 0:
-            self.status = 'idle'
+        if self.attacking:
+            self.status = 'attack'
+            # On stoppe le mouvement pendant l'attaque
+            self.direction.x = 0
+            self.direction.y = 0
         else:
-            self.status = 'run'
+            if self.direction.x == 0 and self.direction.y == 0:
+                self.status = 'idle'
+            else:
+                self.status = 'run'
+
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()
+
+        if self.attacking:
+            # Si le temps écoulé dépasse le cooldown, on finit l'attaque
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
@@ -94,6 +118,7 @@ class Player(pygame.sprite.Sprite):
 
         image_to_show = animation[int(self.frame_index)]
 
+        # Gestion du flip (inchangée)
         if self.direction.x > 0:
             self.facing_right = True
         elif self.direction.x < 0:
@@ -106,6 +131,7 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.input()
+        self.cooldowns()  # ### NOUVEAU : On vérifie les timers
         self.get_status()
         self.move(self.speed)
         self.animate()
